e18e6d71d4364cbd222cf1299052ce3471dbc93f
Jonas Zürcher <jonas.zuercher@spschweiz.ch>
2019-07-13 04:00:49 +0200

    I'm joking.

---
+++ dadjoke.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+topic=$1
+
+    while test $# -gt 0; do
+      case "$1" in
+        --help)
+          echo "/dadjoke - Can I haz dadjoke pleaze?"
+          echo " "
+          echo "Try /dadjoke [topic]"
+          echo " "
+          echo "Stupid jokes. Ask your dad."
+          echo " "
+          echo "Thanks to https://icanhazdadjoke.com"
+          exit 0
+          ;;
+        *)
+          break
+          ;;
+     esac
+    done
+
+
+if [[ -z $topic ]]; then
+          echo "here's a random joke:"
+          echo " "
+          echo $(curl -s -H "Accept: text/plain" "https://icanhazdadjoke.com/")
+          exit 0
+else
+
+    set -f
+    echo $(curl -s -H "Accept: text/plain" "https://icanhazdadjoke.com/search?term="$topic"&limit=1")
+fi
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Dadjokes on Nextcloud Talk
+
+The world was waiting for this: Dadjokes on Nextcloud Talk!
+
+## Installation
+
+Copy dadjoke.sh to your server and follow this guide: https://github.com/nextcloud/spreed/blob/master/docs/commands.md
