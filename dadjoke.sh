b6069e9c418fa63b10db5a51ec9b2c07864d4623
Jonas Zürcher <jonas.zuercher@spschweiz.ch>
2019-07-13 04:23:44 +0200

    Update dadjoke.sh

--- dadjoke.sh
+++ dadjoke.sh
@@ -2,32 +2,34 @@
 
 topic=$1
 
-    while test $# -gt 0; do
-      case "$1" in
-        --help)
-          echo "/dadjoke - I can haz dadjoke?"
-          echo " "
-          echo "Try /dadjoke [topic]"
-          echo " "
-          echo "Stupid jokes. Ask your dad."
-          echo " "
-          echo "Thanks to https://icanhazdadjoke.com"
-          exit 0
-          ;;
-        *)
-          break
-          ;;
-     esac
-    done
+  while test $# -gt 0; do
+    case "$1" in
+      --help)
+        echo "/dadjoke - I can haz dadjoke?"
+        echo " "
+        echo "Try /dadjoke [topic]"
+        echo " "
+        echo "Stupid jokes. Ask your dad."
+        echo " "
+        echo "Thanks to https://icanhazdadjoke.com"
+        exit 0
+        ;;
+      *)
+        break
+        ;;
+   esac
+  done
 
 
-if [[ -z $topic ]]; then
-          echo "here's a random joke:"
-          echo " "
-          echo $(curl -s -H "Accept: text/plain" "https://icanhazdadjoke.com/")
-          exit 0
-else
+  if [[ -z $topic ]]; then
+    echo "here's a random joke:"
+    echo " "
+    echo $(curl -s -H "Accept: text/plain" "https://icanhazdadjoke.com/")
+    exit 0
+
+  else
 
     set -f
     echo $(curl -s -H "Accept: text/plain" "https://icanhazdadjoke.com/search?term="$topic"&limit=1")
-fi
+
+  fi
