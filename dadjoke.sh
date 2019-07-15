6972c8ead7d1ada8a881eb45db2973bb4abfd922
Jonas Zürcher <jonas.zuercher@spschweiz.ch>
2019-07-15 16:41:45 +0200

    Update dadjoke.sh

--- dadjoke.sh
+++ dadjoke.sh
@@ -24,12 +24,12 @@ topic=$1
   if [[ -z $topic ]]; then
     echo "here's a random joke:"
     echo " "
-    echo $(curl -s -H "User-Agent: Icanhazdadjoke for Nextcloud Talk (https://github.com/tsueri/nextcloud-talk-dadjoke)" "Accept: text/plain" "https://icanhazdadjoke.com/")
+    echo $(curl -s -H -A "User-Agent: Icanhazdadjoke for Nextcloud Talk (https://github.com/tsueri/nextcloud-talk-dadjoke)" "Accept: text/plain" "https://icanhazdadjoke.com/")
     exit 0
 
   else
 
     set -f
-    echo $(curl -s -H "User-Agent: Icanhazdadjoke for Nextcloud Talk (https://github.com/tsueri/nextcloud-talk-dadjoke)" "Accept: text/plain" "https://icanhazdadjoke.com/search?term="$topic"&limit=1")
+    echo $(curl -s -H -A "User-Agent: Icanhazdadjoke for Nextcloud Talk (https://github.com/tsueri/nextcloud-talk-dadjoke)" "Accept: text/plain" "https://icanhazdadjoke.com/search?term="$topic"&limit=1")
 
   fi
